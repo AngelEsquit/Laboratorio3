f4aec23b7ccb8eb99a52bf53b25fc185518fe407
AngelEsquit <esq23221@uvg.edu.gt>
2023-10-20 21:33:18 -0600

    Create Dulce.java

---
+++ Dulce.java
@@ -0,0 +1,27 @@
+public class Dulce extends Producto {
+    private String sabor;
+    private String tamanio;
+
+    public Dulce(int id, String nombre, int cantidad_disponible, int cantidad_vendidos, String estado, int precio,
+            String sabor, String tamanio) {
+        super(id, nombre, cantidad_disponible, cantidad_vendidos, estado, precio);
+        this.sabor = sabor;
+        this.tamanio = tamanio;
+    }
+
+    public String getSabor() {
+        return sabor;
+    }
+
+    public void setSabor(String sabor) {
+        this.sabor = sabor;
+    }
+
+    public String getTamanio() {
+        return tamanio;
+    }
+    
+    public void setTamanio(String tamanio) {
+        this.tamanio = tamanio;
+    }
+}
