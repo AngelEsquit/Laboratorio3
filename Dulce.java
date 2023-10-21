28d731d1f23372640cf1c893b2830e5eaade1ba5
AngelEsquit <esq23221@uvg.edu.gt>
2023-10-21 16:30:44 -0600

    Update Dulce.java

--- Dulce.java
+++ Dulce.java
@@ -2,7 +2,7 @@ public class Dulce extends Producto {
     private String sabor;
     private String tamanio;
 
-    public Dulce(int id, String nombre, int cantidad_disponible, int cantidad_vendidos, String estado, int precio,
+    public Dulce(int id, String nombre, int cantidad_disponible, int cantidad_vendidos, String estado, float precio,
             String sabor, String tamanio) {
         super(id, nombre, cantidad_disponible, cantidad_vendidos, estado, precio);
         this.sabor = sabor;
@@ -20,7 +20,7 @@ public class Dulce extends Producto {
     public String getTamanio() {
         return tamanio;
     }
-    
+
     public void setTamanio(String tamanio) {
         this.tamanio = tamanio;
     }
